9bd08a3ad9cef9cbdd716344bf627fe5011b7ece
guilherme <guilhermehen.tassinari@gmail.com>
2024-11-29 01:03:19 -0300

    Created tag component, buttonText component and styled the Details page

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,22 +1,48 @@
-import { Container, Links } from "./styles";
+import { Container, Links, Content } from "./styles";
 
 import { Header } from "../../components/Header";
 import { Button } from "../../components/Button";
-import { Section} from "../../components/Section";
+import { Section } from "../../components/Section";
+import { Tag } from "../../components/Tag";
+import { ButtonText } from "../../components/ButtonText";
 
 export function Details() {
   return (
     <Container>
       <Header />
 
-      <Section title="Useful links">
-        <Links>
-          <li><a href="#">https://www.rocketseat.com.br</a></li>
-          <li><a href="#">https://www.rocketseat.com.br</a></li>
-        </Links>
-      </Section>
+      <main>
+        <Content>
+          <ButtonText title="Delete Note" />
 
-      <Button title="Back" />
+          <h1>Introduction to React</h1>
+
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquid
+            architecto, quo animi enim pariatur, obcaecati ab deserunt quod
+            fugit rerum quae commodi laborum ducimus et aut? Tempore temporibus
+            repudiandae eaque!
+          </p>
+
+          <Section title="Useful links">
+            <Links>
+              <li>
+                <a href="#">https://www.rocketseat.com.br</a>
+              </li>
+              <li>
+                <a href="#">https://www.rocketseat.com.br</a>
+              </li>
+            </Links>
+          </Section>
+
+          <Section title="Bookmarks">
+            <Tag title="express" />
+            <Tag title="node" />
+          </Section>
+
+          <Button title="Back" />
+        </Content>
+      </main>
     </Container>
   );
 }
