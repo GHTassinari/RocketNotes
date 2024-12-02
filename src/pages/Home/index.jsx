d4b9267377269da86aee21c83c25f821c580df92
guilherme <guilhermehen.tassinari@gmail.com>
2024-12-02 00:37:16 -0300

    Created the note inside the home page, created the note component, and ajusted the tag component

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,8 +1,11 @@
 import { FiPlus, FiSearch } from 'react-icons/fi';
 import { Container, Brand, Menu, Search, Content, NewNote } from "./styles";
 
-import { Header } from "../../components/Header";
 import { Input } from "../../components/Input";
+import { Note } from "../../components/Note";
+
+import { Header } from "../../components/Header";
+import { Section } from "../../components/Section";
 import { ButtonText } from "../../components/ButtonText";
 
 export function Home() {
@@ -30,7 +33,17 @@ export function Home() {
         <Input placeholder="Search for the title" icon={FiSearch}/>
       </Search>
 
-      <Content></Content>
+      <Content>
+        <Section title="My notes">
+          <Note data={{ 
+            title: 'React',  
+            tags:[
+              {id: '1', name: 'react'},
+              {id: '2', name: 'rocketseat'}
+            ]
+        }}/>
+        </Section>
+      </Content>
 
       <NewNote>
         <FiPlus/>
