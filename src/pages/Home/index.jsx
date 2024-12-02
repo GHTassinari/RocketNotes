2fc84c6cf85b0567c8bfb84698e5849e00f2a25f
guilherme <guilhermehen.tassinari@gmail.com>
2024-12-01 23:10:00 -0300

    Created an aside bar for the home page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,32 +1,34 @@
-import { Container, Brand, Menu, Search, Content, NewNote } from './styles';
+import { Container, Brand, Menu, Search, Content, NewNote } from "./styles";
 
-import { Header } from '../../components/Header';
+import { Header } from "../../components/Header";
+import { ButtonText } from "../../components/ButtonText";
 
-export function Home(){
-  return(
+export function Home() {
+  return (
     <Container>
       <Brand>
         <h1>Rocketnotes</h1>
       </Brand>
 
-      <Header/>
+      <Header />
 
       <Menu>
-
+        <li>
+          <ButtonText title="All" isActive />
+        </li>
+        <li>
+          <ButtonText title="Reactjs" />
+        </li>
+        <li>
+          <ButtonText title="Nodejs" />
+        </li>
       </Menu>
 
-      <Search>
+      <Search></Search>
 
-      </Search>
+      <Content></Content>
 
-      <Content>
-
-      </Content>
-
-      <NewNote>
-
-      </NewNote>
-
+      <NewNote></NewNote>
     </Container>
   );
 }
