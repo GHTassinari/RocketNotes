f3f5887e0d12ee21dc16495e936cb94f371dc2d2
Guilherme Tassinari <guilhermehen.tassinari@gmail.com>
2025-01-13 00:43:35 -0300

    Inserted the Avatar pfp inside the profile page

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,49 +1,41 @@
-import { FiArrowLeft, FiUser, FiMail, FiLock } from 'react-icons/fi';
+import { FiArrowLeft, FiUser, FiMail, FiLock, FiCamera } from "react-icons/fi";
 
-import { Input } from '../../components/Input';
-import { Button } from '../../components/Button';
+import { Input } from "../../components/Input";
+import { Button } from "../../components/Button";
 
-import { Container, Form } from "./styles";
+import { Container, Form, Avatar } from "./styles";
 
-export function Profile(){
-    return(
-        <Container>
-            <header>
-                <a href="/">
-                    <FiArrowLeft />
-                </a>
-            </header>
+export function Profile() {
+  return (
+    <Container>
+      <header>
+        <a href="/">
+          <FiArrowLeft />
+        </a>
+      </header>
 
-            <Form>
-                <Input
-                    placeholder="Name"
-                    type="text"
-                    icon={FiUser}
+      <Form>
+        <Avatar>
+            <img src="https://github.com/GHTassinari.png" alt="User photo"></img>
+
+            <label htmlFor="avatar">
+                <FiCamera />
+                <input
+                    id="avatar"
+                    type="file"
                 />
+            </label>
+        </Avatar>
+        <Input placeholder="Name" type="text" icon={FiUser} />
 
-                <Input
-                    placeholder="E-mail"
-                    type="text"
-                    icon={FiMail}
-                />
+        <Input placeholder="E-mail" type="text" icon={FiMail} />
 
-                <Input
-                    placeholder="Current Password"
-                    type="password"
-                    icon={FiLock}
-                />
+        <Input placeholder="Current Password" type="password" icon={FiLock} />
 
-                <Input
-                    placeholder="New Password"
-                    type="password"
-                    icon={FiLock}
-                />
+        <Input placeholder="New Password" type="password" icon={FiLock} />
 
-                <Button 
-                title="Save changes"
-                />
-            </Form>
-
-        </Container>
-    )
+        <Button title="Save changes" />
+      </Form>
+    </Container>
+  );
 }
