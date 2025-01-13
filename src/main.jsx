a38bd6cd325679faac1ae5a8fac1edea9aeb7073
Guilherme Tassinari <guilhermehen.tassinari@gmail.com>
2025-01-13 00:27:36 -0300

    Adding the profile page

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,13 @@ import { ThemeProvider } from 'styled-components';
 import GlobalStyles from './styles/global';
 import theme from './styles/theme'
 
-import { SignUp } from './pages/SignUp'
+import { Profile } from './pages/Profile'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <SignUp />
+      <Profile/>
     </ThemeProvider>
   </StrictMode>,
 )
