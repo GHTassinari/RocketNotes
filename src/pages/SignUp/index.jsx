b6da6b30602569293ff707439d51e5a39dcba3c8
Guilherme Tassinari <guilhermehen.tassinari@gmail.com>
2025-03-23 23:21:38 -0300

    Implement user signup functionality with API integration

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,5 +1,8 @@
+import { useState } from "react";
 import { FiMail, FiLock, FiUser } from "react-icons/fi";
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+
+import { api } from "../../services/api";
 
 import { Input } from "../../components/Input";
 import { Button } from "../../components/Button";
@@ -7,6 +10,31 @@ import { Button } from "../../components/Button";
 import { Container, Form, Background } from "./styles";
 
 export function SignUp() {
+  const [ name, setName ] = useState("");
+  const [ email, setEmail ] = useState("");
+  const [ password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+
+  function handleSignUp(){
+    if(!name || !email || !password) {
+      alert("Please, fill in all fields");
+    }
+
+    api.post("/users", {
+      name, email, password
+    }).then(() => {
+      alert("The user account was created successfully");
+      navigate("/");
+    }).catch(error => {
+      if(error.response){
+        alert(error.response.data.message);
+      } else {
+        alert("Unable to create the account")
+      }
+    })
+  }
+
   return (
     <Container>
       <Background />
@@ -16,13 +44,13 @@ export function SignUp() {
 
         <h2>Create your account</h2>
 
-        <Input placeholder="Name" type="text" icon={FiUser} />
+        <Input placeholder="Name" type="text" icon={FiUser} onChange={e => setName(e.target.value)}/>
 
-        <Input placeholder="E-mail" type="text" icon={FiMail} />
+        <Input placeholder="E-mail" type="text" icon={FiMail} onChange={e => setEmail(e.target.value)} />
 
-        <Input placeholder="Password" type="password" icon={FiLock} />
+        <Input placeholder="Password" type="password" icon={FiLock}  onChange={e => setPassword(e.target.value)}/>
 
-        <Button title="Sign up" />
+        <Button title="Sign up" onClick = {handleSignUp}/>
 
         <Link to="/">
           Already have an account?
